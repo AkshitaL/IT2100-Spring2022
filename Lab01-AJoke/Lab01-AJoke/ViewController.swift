c5faf254103798278db5315c4834000e12b60478
Akshita Lathar <akshitalathar@akshitas-mbp.lan>
2022-01-28 21:12:58 -0500

    Adding Lab 1

---
+++ Lab01-AJoke/Lab01-AJoke/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Lab01-AJoke
+//
+//  Created by Akshita Lathar on 1/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var jokeLabel: UILabel!
+    @IBOutlet var punchLineLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showJoke(_ sender: UIButton) {
+        jokeLabel.text = "The first byte asked the second byte, \"are you feeling sick?\""
+    }
+
+    @IBAction func showPunchLine(_ sender: UIButton) {
+        punchLineLabel.text = "No, I'm just feeling a bit off."
+    }
+}
+
