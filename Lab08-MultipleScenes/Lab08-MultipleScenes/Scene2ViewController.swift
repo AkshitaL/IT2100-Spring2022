cd90f532550fc2c6298855fb83deeb7252026ad7
Akshita Lathar <akshitalathar@akshitas-mbp.lan>
2022-03-13 20:28:49 -0400

    Adding Lab 8

---
+++ Lab08-MultipleScenes/Lab08-MultipleScenes/Scene2ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Scene2ViewController.swift
+//  Lab08-MultipleScenes
+//
+//  Created by Akshita Lathar on 3/13/22.
+//
+
+import UIKit
+
+class Scene2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Lab08-MultipleScenes/Lab08-MultipleScenes/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Lab08-MultipleScenes
+//
+//  Created by Akshita Lathar on 3/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwind( _ seg: UIStoryboardSegue) {
+    }
+
+
+}
+
