e395d08a7b1b60b2e0259522368026a83098e42e
Akshita Lathar <akshitalathar@akshitas-mbp.lan>
2022-04-18 21:08:47 -0400

    Adding Lab 12

---
+++ Lab12-PresentView/Lab12-PresentView/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Lab12-PresentView
+//
+//  Created by Akshita Lathar on 4/18/22.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+        view.backgroundColor = .systemBlue
+    }
+    
+
+}
---
+++ Lab12-PresentView/Lab12-PresentView/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Lab12-PresentView
+//
+//  Created by Akshita Lathar on 4/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    private let button: UIButton = {
+        let button = UIButton(frame: CGRect(x:0, y:0, width: 200, height: 50))
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemRed
+        button.setTitle("Tap me", for: .normal)
+        return button
+    }()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(button)
+        button.center = view.center
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    }
+
+    @objc private func didTapButton() {
+        let vc = SecondViewController()
+        vc.modalTransitionStyle = .crossDissolve
+        present(vc, animated: true)
+    }
+
+}
+
